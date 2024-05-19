90500eeb749d85945ac1a1f17f48c1c27af299b6
Ryan Xu <ryan.y.xu@hotmail.com>
2024-05-18 21:27:26 -0400

    Replace the "Click me" text by algonquin.jpg

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool isChecked = false;
+  late TextEditingController _controller; // for user Strings
+    //initialize later, but not null
+
+  @override
+  void initState() {  //loading page
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {  // unloading page
+    _controller.dispose();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -51,12 +66,21 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many times:',style: TextStyle(fontSize: 20)
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium, ),
+            Image.asset("images/algonquin.jpg", width:300.0, height:200.0),
+
+            // ElevatedButton(onPressed:buttonClicked, child: Text("Click me")),
+            ElevatedButton(onPressed:buttonClicked, child: Image.asset("images/algonquin.jpg", width:200.0, height:150.0)),
+            Checkbox(value: isChecked, onChanged: checkBoxClicked),
+            Switch(value: isChecked, onChanged: checkBoxClicked),
+            TextField(controller: _controller,
+                decoration: InputDecoration(
+                hintText:"Type here",
+                border: OutlineInputBorder(),
+                labelText: "First name"
+            )),
           ],
         ),
       ),
@@ -67,4 +91,29 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  // function for checkBoxClicked
+  void checkBoxClicked(bool ? newVal) // ? newVal is potentially null
+  {
+    if(newVal != null) {
+      setState((){
+        isChecked = newVal!;  // update the GUI, ! assert not null
+      });
+    }
+  }
+
+  // function for buttonClicked
+  void buttonClicked(){
+      var userTyped = _controller.value.text;
+  }
+
+  // function to set NewValue
+  void setNewValue(double newVal){
+    setState(){
+
+    }
+  }
+
+
+
 }
