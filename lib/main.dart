0b7a562f4a50b5632dd16526d60223f27ec35b16
Ryan Xu <ryan.y.xu@hotmail.com>
2024-05-31 10:14:59 -0400

    SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,12 +37,56 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   bool isChecked = false;
   late TextEditingController _controller; // for user Strings
+  late TextEditingController _controller2; // for user Strings
+  late SharedPreferences storedData;
     //initialize later, but not null
+  // storeData = SharedPreferences();
+  // storeData = EncryptedSharedPreferences();
+
+  //Asynchronous
+  // void loadSavedVariables() async{
+  //   // This is asynchronous, await is used with asynchronous operations
+  //   //prefs should be a map of key -value
+  //   final prefs = await SharedPreferences.getInstance();
+  //   // Asynchronous
+  //   var otherPrefs = await SharedPreferences.getInstance()
+  //     .then((thePrefs){
+  //       // thePrefs have been loaded
+  //   });// at this point, the file has been loaded
+  // }
+
+
+  // void loadSavedVariables() { // Not asynchronous
+  //   var otherPrefs = SharedPreferences.getInstance()
+  //       .then((thePrefs){
+  //     // thePrefs have been loaded
+  //   });// at this point, the file has been loaded
+  // }
+
+  void loadSavedVariables() { // Not asynchronous
+
+    // at this point, the file has been loaded
+  }
+
 
   @override
   void initState() {  //loading page
     super.initState();
     _controller = TextEditingController(); // Initialize the controller here
+    _controller2 = TextEditingController();
+
+    SharedPreferences.getInstance().then((thePrefs){
+      // at this point, the file (thePrefs) have been loaded
+      storedData = thePrefs;
+
+      var savedUserName = storedData.getString("UserName"); //Nullable string
+
+      if(savedUserName != null){
+        _controller.text = savedUserName;
+      }
+
+    });
+
   }
 
   @override
@@ -54,6 +100,14 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+
+
+  // flutter pub outdated
+  // flutter pub upgrade --major-versions
+  // flutter pub add shared_preferences
+  // flutter pub add encrypted_shared_preferences
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -88,8 +142,17 @@ class _MyHomePageState extends State<MyHomePage> {
             // crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              getText(),
+              getOtherText(),
               Text("Click the button below", style: TextStyle(fontSize: 30.0, color: Colors.redAccent),),
-              ElevatedButton( onPressed: buttonClicked, child: Text("Click here"),)
+              ElevatedButton( onPressed: buttonClicked, child: Text("Click here"),),
+
+              TextField(controller: _controller,
+                decoration: InputDecoration(
+                hintText: "Type here",
+                border: OutlineInputBorder(),
+                labelText: "Login name"
+              ),)
 
         ])
       ),
@@ -111,34 +174,40 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  Widget getText() {
+    return Text("I'm a text"); //simple retrun /getter funtion
+  }
+  Widget getOtherText() => Text("I'm another Text");
+
   // function for buttonClicked
   void buttonClicked(){
       // var userTyped = _controller.value.text;
       // _controller.text = "You typed: " + userTyped;
 
-      // var mySB = SnackBar( content:
-      //   Row(
-      //     children: [Image.asset("images/algonquin.jpg", width: 100, height: 100),
-      //     Text('Yay! A SnackBar!'), ],
-      //   ),
-      //     action:SnackBarAction( label:'Ok', onPressed: () {  } ),
-      // );
-      //
-      // ScaffoldMessenger.of(context).showSnackBar(mySB); // This show the SnackBAr
-
-    Widget getText() {
-      return Text("I'm a text"); //simple retrun /getter funtion
-    }
-    Widget getOtherText() => Text("I'm another Text");
+      var mySB = SnackBar( content:
+        Row(
+          children: [Image.asset("images/algonquin.jpg", width: 100, height: 100),
+          Text('Yay! A SnackBar!'), ],
+        ),
+          action:SnackBarAction( label:'Ok', onPressed: () {  } ),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(mySB); // This show the SnackBAr
 
     showDialog<String>(
       context: context,
       builder: (BuildContext context) =>
           AlertDialog(
-            title: const Text('AlertDialog Title'),
-            content: const Text('AlertDialog description'),
+            title: const Text('Save data'),
+            content: const Text('Do you want to save your information?'),
             actions: <Widget>[
-              ElevatedButton(child:Text("Ok"), onPressed: (){},),
+              ElevatedButton(child:Text("Ok"), onPressed: (){
+                var userTyped = _controller.value.text;
+                storedData.setString("UserName", userTyped);
+
+                Navigator.pop(context);
+
+              },),
+              // ElevatedButton(onPressed: (){ Navigator.pop(context);}, child: Text("Ok")),
               FilledButton(onPressed: (){ Navigator.pop(context);}, child: Text("Cancel")),
               OutlinedButton(onPressed: (){ Navigator.pop(context);}, child: Text("Delete")),
               Image.asset("images/algonquin.jpg", width:100, height: 100),
